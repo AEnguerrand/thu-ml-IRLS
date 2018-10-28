edb1b1a7d26dbede6749a93585a95b2f22e7bc51
Enguerrand <contact@engue0774.fr>
2018-10-28 11:43:13 +0800

    First version work

--- irls.py
+++ irls.py
@@ -16,7 +16,6 @@ def parser(filepath):
     with open(filepath, 'r') as file:
         lines = file.readlines()
     x = numpy.zeros((len(lines), 124))
-    x[:, 0] = 1
     y = numpy.ones(len(lines))
     i = 0
     for line in lines:
@@ -25,33 +24,38 @@ def parser(filepath):
         data = data[1:]
         for dataFeat in data:
             feat = dataFeat.split(':')
-            x[i, int(feat[0])] = 1
+            x[i, int(feat[0])] = 1.0
         i += 1
     return x, y
 
 
 # IRLS algo
-def irls(x, y):
+def irls(x, y, lam):
     i = 0
     w = numpy.zeros(x.shape[1])
     w0 = numpy.log(numpy.average(y) / (1 - numpy.average(y)))
     while i < maxIt:
+        print("IRLS: IT")
         ni = w0 + x.dot(w)
         ui = sigm(ni)
-        si = ui * (1 - ui)
+        si = ui * (1.0 - ui)
         zi = ni + ((y - ui) / si)
         S = numpy.diag(si)
         u = S.dot(zi)
-        w = numpy.power((x.transpose().dot(S).dot(x)), -1) * x.transpose().dot(S).dot(zi)
+        mIdit = numpy.identity(x.shape[1])
+        w = numpy.linalg.inv((x.transpose().dot(S).dot(x)) + (mIdit * lam)).dot(x.transpose().dot(S).dot(zi))
         print(w)
         i += 1
     return i, w, ui
 
 
 # Parser dataset
+print("Start program ...")
 x, y = parser(datasetPath)
+print("File parser")
 # Parser IRLS
-i, w, ui = irls(x, y)
+i, w, ui = irls(x, y, 1)
+print("IRLS run")
 # Print
 u = 0
 while u < len(y):
